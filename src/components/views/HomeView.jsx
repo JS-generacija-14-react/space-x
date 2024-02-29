3c107d68acf1247ba4d5cf144e11f0a2194c1df0
Nebojsa Markovic <nebojsa91markovic@gmail.com>
2024-02-29 22:04:04 +0100

    added loading and error components

--- src/components/views/HomeView.jsx
+++ src/components/views/HomeView.jsx
@@ -1,18 +1,37 @@
 import { useState, useEffect } from "react";
 import instance from "../../services/api";
 import Company from "../company/Company";
+import Loading from "../loading/Loading";
+import Error from "../error/Error";
 
 const HomeView = () => {
   const [companyInfo, setCompanyInfo] = useState(undefined);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    instance.get("company").then((res) => setCompanyInfo(res.data));
+    instance
+      .get("company")
+      .then((res) => setCompanyInfo(res.data))
+      .catch((error) => {
+        setError(true);
+        console.error(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
 
   return (
     <>
       <h1>HomeView</h1>
-      {companyInfo ? <Company companyInfo={companyInfo} /> : null}
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <Error />
+      ) : (
+        <Company companyInfo={companyInfo} />
+      )}
     </>
   );
 };
--- src/components/views/LaunchesView.jsx
+++ src/components/views/LaunchesView.jsx
@@ -1,17 +1,35 @@
 import { useState, useEffect } from "react";
 import instance from "../../services/api";
 import LaunchesList from "../launches/LaunchesList";
+import Loading from "../loading/Loading";
+import Error from "../error/Error";
 
 const LaunchesView = () => {
   const [launches, setLaunches] = useState(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    instance.get("/launches").then((res) => setLaunches(res.data.slice(0, 10)));
+    instance
+      .get("/launches")
+      .then((res) => setLaunches(res.data.slice(0, 10)))
+      .catch((error) => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, []);
   return (
     <>
       <h1>LaunchesView</h1>
-      {launches ? <LaunchesList launches={launches} /> : null}
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <Error />
+      ) : (
+        <LaunchesList launches={launches} />
+      )}
     </>
   );
 };
